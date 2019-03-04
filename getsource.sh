c632efa5cb3da214fa58a245b2ba47f6a4fedf13
Heikki Pernu <pernu@fmi.fi>
2019-03-04 11:26:59 +0200

    Working for one module

--- getsource.sh
+++ getsource.sh
@@ -12,7 +12,18 @@ for i in CI CIRCLECI CIRCLE_BRANCH CIRCLE_REPOSITORY_URL CIRCLE_WORKING_DIRECTOR
 	if [ -z "$$i" ] ; then
 		echo "Environment variable $i needs to be set and non-empty" >&2
 		exit 2
+	else
+		echo "$i: $$i"
 	fi
 done
 
 # Assume /tmp/build is unique to this container
+cd /tmp
+mkdir -p build
+jobbase=`echo $CIRCLE_JOB | sed -e 's/^.*smartmet-/smartmet-/'`
+repo=`echo $CIRCLE_REPOSITORY_URL | sed -e "s%[^/]*\$%$jobbase%"`
+echo "Checking out $repo"
+# Check out same branch first, fallback to devel and then to master
+git clone -b "$CIRCLE_BRANCH" "$repo" build || 
+git clone -b devel "$repo" build ||
+git clone -b master "$repo" build
