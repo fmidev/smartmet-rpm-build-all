f5e13c8894d096485f520ae1314dd22cce887474
Mika Heiskanen <mika.heiskanen@fmi.fi>
2020-11-30 15:48:24 +0200

    Commented out non-working error handling

--- getsource.sh
+++ getsource.sh
@@ -28,13 +28,16 @@ fi
 
 # Assume /tmp/build is unique to this container
 cd /tmp
-mkdir -p build
 jobbase=`echo $CIRCLE_JOB | sed -e 's/^.*smartmet-/smartmet-/'`
 repo="$repobase/$jobbase"
 echo "Checking out $repo"
-# Check out same branch first, fallback to devel and then to master
-git clone -b "$CIRCLE_BRANCH" "$repo" build || 
-git clone -b devel "$repo" build ||
-git clone -b master "$repo" build
+
+# Check out same branch first, fallback to devel and then to master.
+
+# THIS DOES NOT WORK, SINCE 'build' may have been created. Hence we try only the requested branch for now:
+#
+# git clone -b "$CIRCLE_BRANCH" "$repo" build || git clone -b devel "$repo" build || git clone -b master "$repo" build
+git clone -b "$CIRCLE_BRANCH" "$repo" build
+
 cd build
 echo Checked out branch "`git branch --no-color | cut -f 2 -d ' '`"
